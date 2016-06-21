efa58ea9d8b39a6c64887f6feeabacde643116f4
Nick Tolkachev <n7olkachev@gmail.com>
2016-06-21 19:20:26 +0300

    Update SearchableTrait.php

--- src/SearchableTrait.php
+++ src/SearchableTrait.php
@@ -77,6 +77,7 @@ trait SearchableTrait
         $items = self::$elasticsearch->search([
             'index' => array_get(self::$config, 'index'),
             'type' => array_get(self::$config, 'type'),
+            'size' => array_get(self::$config, 'size'),
             'body' => [
                 'query' => $query,
             ]
