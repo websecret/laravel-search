b2b04edcce645c649ff8b91be3fc6a59d6e6a77c
Vladilen Chernyavski <blax56@gmail.com>
2016-08-30 13:08:44 +0300

    Update SearchableTrait.php

--- src/SearchableTrait.php
+++ src/SearchableTrait.php
@@ -41,6 +41,7 @@ trait SearchableTrait
 
     protected function getItems($search, $options = [])
     {
+        $search = addcslashes($search, '.?*|{}[]()"\\');
         if (array_get($options, 'wildcard', false)) {
             return self::$elasticsearch->search([
                 'index' => array_get(self::$config, 'index'),
@@ -56,7 +57,6 @@ trait SearchableTrait
                 ]
             ]);
         }
-        $search = addcslashes($search, '.?*|{}[]()"\\');
         if ($fields = array_get(static::$config, 'fields')) {
             $matchFields = [];
             $match = [
