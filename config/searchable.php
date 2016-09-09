284b09a9fa45ae6c7afdb4f98403d6db417b7427
Vladilen <vlad@websecret.by>
2016-09-09 14:12:24 +0300

    add hosts to env

--- config/searchable.php
+++ config/searchable.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'hosts' => explode('|', ENV('ELASTICSEARCH_HOSTS', [])),
+    'hosts' => explode('|', ENV('ELASTICSEARCH_HOSTS', '')),
     'index' => ENV('ELASTICSEARCH_INDEX', 'index'),
     'fuzziness' => 2,
     "prefix_length" => 2,
