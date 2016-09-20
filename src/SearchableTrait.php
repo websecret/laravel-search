5e3a0272527ed06626edea0e89f6372fe674d492
Vladilen Chernyavski <blax56@gmail.com>
2016-09-20 18:30:45 +0300

    fixed slash escape

--- src/SearchableTrait.php
+++ src/SearchableTrait.php
@@ -51,7 +51,7 @@ trait SearchableTrait
 
     protected function getItems($search, $options = [])
     {
-        $search = addcslashes($search, '.?|{}[]()"\\');
+        $search = addcslashes($search, '.?|{}[]()"\\/');
         if (array_get($options, 'wildcard', false)) {
             return self::$elasticsearch->search([
                 'index' => array_get(self::$config, 'index'),
